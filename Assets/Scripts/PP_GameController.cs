9a4d89d907badc9c2f389b20e07cd9b9e01b0063
Asema Hassan <asema.hassan@gmail.com>
2015-04-18 22:11:47 +0200

    Watersplash sound added and game lose banner.

--- Assets/Scripts/PP_GameController.cs
+++ Assets/Scripts/PP_GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PP_GameController : MonoBehaviour {
@@ -6,42 +7,56 @@ public class PP_GameController : MonoBehaviour {
 
 	public int currentLevel =1;
 	public int score=0;
-	
+	public GameObject bannerObj;
+
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
 	// user methods for game status
-	void updateGameLevels(){
+	public void updateGameLevels(){
 		// increment game level when player has reached ISLAND
 
 		// when level has been incremented, switch to new level enable
 	}
 
 
-	void gameCompletedStatus(){
+	public void gameCompletedStatus(){
 		// check if the game has completed and what are the scores to be shown on HUD
-
 	}
 
-	public void gameEndStatus()
-	{
-		// TODO messages ... 
+	void animateTheBannerMessage(string msg){
+		bannerObj.SetActive (true);
+		//bannerObj.transform.localScale = new Vector3 (0.1f,0.1f,0.1f);
+		GameObject textMsg = bannerObj.transform.Find("Message").gameObject;
+		textMsg.GetComponent<Text>().text = msg;
+	}
 
+	void resetTheGameState(){
+
+		//disable the banner
+		bannerObj.SetActive (false);
 
 		// Reset player
 		var players = GameObject.FindObjectsOfType<Player>();
 		foreach (var player in players)
 			player.Reset();
-
+		
 		// Reset all floes
 		var allIceFloes = GameObject.FindObjectsOfType<IceFloe>();
 		foreach (var iceFloe in allIceFloes)
 			iceFloe.Reset();
 	}
 
-	void updatePlayerScore(){
+	public void gameEndStatus()
+	{
+		// TODO messages ... 
+		animateTheBannerMessage ("You've lost it!");
+		Invoke ("resetTheGameState", 1.0f);
+	}
+
+	public void updatePlayerScore(){
 
 	}
 
--- Assets/Scripts/WaterCollision.cs
+++ Assets/Scripts/WaterCollision.cs
@@ -5,8 +5,17 @@ public class WaterCollision : MonoBehaviour {
 
 
 	public GameObject waterSplash;
+	public AudioClip waterSplashSfx;
+	public AudioSource aSource;
+
 	void OnTriggerEnter(Collider collidedObj){
+
 		if (collidedObj.gameObject.name == "Player"){
+
+			//play water splash sound
+			aSource.clip = waterSplashSfx;
+			aSource.Play();
+
 			// instantiate a particle system of water splash
 			GameObject splash = Instantiate(waterSplash,collidedObj.transform.localPosition,Quaternion.identity) as GameObject;
 			ParticleSystem ps = splash.GetComponent<ParticleSystem>();
@@ -15,10 +24,11 @@ public class WaterCollision : MonoBehaviour {
 		}
 
 	void OnTriggerStay(Collider collidedObj){
+
 		if (collidedObj.gameObject.name == "Player") {
 			//player died, call game lose method of gamecontroller
 			PP_GameController gameCtrl = Camera.main.GetComponent<PP_GameController>();
-			//gameCtrl.gameEndStatus();
+			gameCtrl.gameEndStatus();
 		}
 	}
 
