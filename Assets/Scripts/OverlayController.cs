e043baf2b9bd402654dbbd5cd4099a5ff11ab8df
YsenGrimm <kloeten.wars@googlemail.com>
2015-04-19 14:09:18 +0200

    Tut overlay with show and hide in overlay controller

---
+++ Assets/Scripts/OverlayController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OverlayController : MonoBehaviour {
+
+	public Image overlay;
+	public Image swipe;
+	public Image tap;
+	public Button closeBtn;
+	public Image closeImg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showOverlay() {
+		overlay.enabled = true;
+		swipe.enabled = true;
+		tap.enabled = true;
+		closeBtn.enabled = true;
+		closeImg.enabled = true;
+	}
+
+	public void hideOverlay() {
+		overlay.enabled = false;
+		swipe.enabled = false;
+		tap.enabled = false;
+		closeBtn.enabled = false;
+		closeImg.enabled = false;
+	}
+
+}
